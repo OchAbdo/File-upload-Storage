ada1eb5f314d65fe3ad044a3ee129f317ab88022
OchAbdo <ochabdo02@gmail.com>
2026-01-30 11:37:41 +0100

    add the limit size file and type

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,27 +1,39 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, FileTypeValidator, MaxFileSizeValidator, ParseFilePipe, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 
 @Controller('files')
 export class FilesController {
-    constructor(private filesService: FilesService){}
+    constructor(private filesService: FilesService) { }
 
     @Post('upload')
     @UseInterceptors(
         FileInterceptor('file', {
             storage: diskStorage({
                 destination: './uploads',
-                filename:(req , file, callback)=>{
+                filename: (req, file, callback) => {
                     callback(
                         null,
                         `${Date.now()}-${file.originalname}`
                     )
                 }
-            })
+            }),
+            limits: { fileSize: 5 * 1024 * 1024 },
+            fileFilter: (req, file, callback) => {
+                if (!file.mimetype.match(/(jpg|jpeg|png)$/)) {
+                    return callback(new BadRequestException('Invalide File type'), false)
+                }
+                callback(null, true)
+            },
         })
     )
-    uploadFile(@UploadedFile() file: Express.Multer.File){
+    uploadFile(@UploadedFile(/* new ParseFilePipe({
+        validators: [
+            new MaxFileSizeValidator({ maxSize: 500000 }),
+            new FileTypeValidator({ fileType: 'image/jpeg' }) 
+        ]
+    }) */) file: Express.Multer.File) {
         return this.filesService.uploadLocal(file)
     }
 }
