4c038ccecd3bef626fab567c8c0964266520ae12
OchAbdo <ochabdo02@gmail.com>
2026-02-02 09:24:10 +0100

    install awsSdk and uuid and implement the endpoint

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -36,4 +36,27 @@ export class FilesController {
     }) */) file: Express.Multer.File) {
         return this.filesService.uploadLocal(file)
     }
+
+
+
+
+    @Post("uploads3")
+    @UseInterceptors(
+        FileInterceptor("file", {
+            limits: {
+                fileSize: 5 * 1024 * 1024,
+            },
+            fileFilter(req, file, callback) {
+                if (!file.mimetype.match(/(jpg|jpeg|png)$/)) {
+                    return callback(new Error("Invalid file type"), false);
+                }
+                callback(null, true);
+            },
+        }),
+    )
+    upload(@UploadedFile() file: Express.Multer.File) {
+        return this.filesService.uploadFile(file);
+    }
+
+
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,5 +1,7 @@
+import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { ConflictException, Injectable } from '@nestjs/common';
-
+import { S3client } from 'src/config/minio.config';
+import {v4 as uuid} from "uuid"
 @Injectable()
 export class FilesService {
 
@@ -15,4 +17,30 @@ export class FilesService {
             url: `http://localhost:3000/uploads/${file.filename}`
         }
     }
+
+
+    async uploadFile(file: Express.Multer.File) {
+    const fileKey = `${uuid()}-${file.originalname}`;
+
+    await S3client.send(
+      new PutObjectCommand({
+        Bucket: "uploads",
+        Key: fileKey,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+        Metadata: {
+          originalName: file.originalname,
+          size: file.size.toString(),
+        },
+      }),
+    );
+
+    return {
+      message: "File uploaded successfully",
+      fileName: fileKey,
+      size: file.size,
+      type: file.mimetype,
+    };
+  }
+
 }
